fe095910448d1e9520ebf66996e392f31f622a2e
Denexter3 <Pitekolo030@gmail.com>
2018-08-30 21:20:40 +0200

    Added things, probably bugs

--- main.cpp
+++ main.cpp
@@ -13,16 +13,24 @@ void drawer(int X, int Z, int OX, int OZ, short wpis);
 
 int main()
 {
+    triD kropka;
+    kropka.cordZ=0;
     int sizz, legn;
     short wpisana;
-    triD kropka;
+    std::cout << "First. Choose the length of your road: ";
+    std::cin >> legn;
+    std::cout << "\n Good.\nNow - ammount of lanes on your road: ";
+    std::cin >> sizz;
+    std::cout << "\nTwo more things - which lane do you want to focus on: ";
+    std::cin >> wpisana;
+    std::cout << "\nThank god, thats all - a car can occupy one lane, choose which lane: ";
+    std::cin >> kropka.cordX;
     bool game=true;
-    kropka.cordZ=1;
 
     while(game){
         std::cout << "\033[2J\033[1;1H";
         drawer(sizz, legn, kropka.cordX, kropka.cordZ, wpisana);
-        usleep(1000000);
+        usleep(500000);
         kropka.cordZ+=1;
     }
     return 0;
