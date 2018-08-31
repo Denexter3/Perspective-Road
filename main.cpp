499d2dad3320012f5bb40af3594260ab4c5286cf
Denexter3 <Pitekolo030@gmail.com>
2018-08-31 18:09:51 +0200

    Everyone b saying comments are important
    Borders on the last and first lane, comments on every line of code, you know the normal progranny things. progranny, kek

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h> // Only for usleep, nothing else
 
-class triD
+class triD  // Class for the "car" thingy
 {
 public:
     int cordX;
-    int cordY;
+    int cordY; // At first this program wasgoing to support all X Y Z coordinates. Now it supports only X and Z
     int cordZ;
 };
 
@@ -29,29 +29,31 @@ int main()
     std::cin >> kropka.cordX;
     bool game=true;
 
-    while(game){
+    while(game){ // Game is true as long as you dont add something that will change that
         std::cout << "\033[2J\033[1;1H";
         drawer(sizz, legn, kropka.cordX, kropka.cordZ, wpisana);
-        usleep(500000);
+        usleep(500000); //One frame is on the screen for half a second
         kropka.cordZ+=1;
+        if (kropka.cordZ>legn) kropka.cordZ=0;
     }
     return 0;
 }
 
 void drawer(int X, int Z, int OX, int OZ, short wpis)
 {
-    short dzmien=(-wpis)+1;
-    short pzmien=(-dzmien-2)*Z+dzmien;
+    short dzmien=(-wpis);  // This one controls which lane you focus on
+    short pzmien=(-dzmien-2)*Z+dzmien; // This controls the shift of the road on the X axis
     int przyblizenie=0;
     int oddalenie;
     oddalenie=Z*2+pzmien;
-    for(int bZ=1; bZ<=Z; bZ++){
+    for(int bZ=1; bZ<=Z; bZ++){  // The "Z" dimension
         przyblizenie+=1;
         oddalenie=oddalenie+dzmien;
         std::cout << "\n";
         bool wykonane=false;
         int h=1;
-        for(int bX=1; bX<=X; bX++){
+        for(int bX=0; bX<=X+1; bX++){ // The "X" dimension with one additional phantom lane
+            if(bZ==1) continue; // The first line of the road is not drawn
             if(wykonane==false)
                 {
                     for(int b=1; b<=oddalenie; b++)
@@ -61,8 +63,9 @@ void drawer(int X, int Z, int OX, int OZ, short wpis)
                 }
             wykonane=true;
             if((OX==bX) && (OZ==bZ)) std::cout << "0";
+            else if(bX==0 || bX==X+1) std::cout << "B"; // Draws borders on two "phantom" lanes on the road
             else std::cout << ".";
-            if(h<=X-1)
+            if(h<=X+1) // +1 adds last phantom lane the perspecitive making process
                 {
                     for(int g=1; g<przyblizenie; g++) std::cout << " ";
                 }
